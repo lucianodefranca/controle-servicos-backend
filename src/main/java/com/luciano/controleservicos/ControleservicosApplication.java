ac3cd22f5f6758a0bdb57211777e88fdd8c19795
luciano de franca <lucianodefranca86@gmail.com>
2022-02-18 23:34:39 -0300

    Inserindo cliente na base de dados

--- src/main/java/com/luciano/controleservicos/ControleservicosApplication.java
+++ src/main/java/com/luciano/controleservicos/ControleservicosApplication.java
@@ -1,10 +1,23 @@
 package com.luciano.controleservicos;
 
+import com.luciano.controleservicos.model.entities.Cliente;
+import com.luciano.controleservicos.model.repositories.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ControleservicosApplication {
+
+	@Bean
+	public CommandLineRunner run(@Autowired ClienteRepository repository) {
+		return args -> {
+			Cliente cliente = Cliente.builder().cpf("12345678910").nome("Luciano").build();
+			repository.save(cliente);
+		};
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ControleservicosApplication.class, args);
--- src/main/java/com/luciano/controleservicos/model/entities/Cliente.java
+++ src/main/java/com/luciano/controleservicos/model/entities/Cliente.java
@@ -1,12 +1,18 @@
 package com.luciano.controleservicos.model.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.time.LocalDate;
 
 @Entity
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Cliente {
 
     @Id
