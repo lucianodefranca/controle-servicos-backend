57498fde1d2189988c33eafb94b824237d85389f
luciano de franca <lucianodefranca86@gmail.com>
2022-02-18 23:13:46 -0300

    Projeto iniciado

---
+++ src/main/java/com/luciano/controleservicos/ControleservicosApplication.java
@@ -0,0 +1,13 @@
+package com.luciano.controleservicos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleservicosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleservicosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luciano/controleservicos/ControleservicosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luciano.controleservicos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleservicosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
