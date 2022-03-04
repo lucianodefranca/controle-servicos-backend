68895eb733caaf641b3b6c917db9163c80e687bd
luciano de franca <lucianodefranca86@gmail.com>
2022-03-04 20:58:25 -0300

    Metodo PasswordEncoder

--- src/main/java/com/luciano/controleservicos/config/ResourceServerConfig.java
+++ src/main/java/com/luciano/controleservicos/config/ResourceServerConfig.java
@@ -1,7 +1,10 @@
 package com.luciano.controleservicos.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
@@ -18,5 +21,10 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
                         "/clientes/**",
                                   "/servicos-prestados").authenticated()
                 .anyRequest().denyAll();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
 }
