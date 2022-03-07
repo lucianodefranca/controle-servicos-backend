6ab658533d7f16a2f5656a11bd4fed684e585c84
luciano de franca <lucianodefranca86@gmail.com>
2022-03-06 23:15:53 -0300

    CroosOrigin para usuarios

--- src/main/java/com/luciano/controleservicos/resources/UsuarioResource.java
+++ src/main/java/com/luciano/controleservicos/resources/UsuarioResource.java
@@ -4,10 +4,7 @@ import com.luciano.controleservicos.model.entities.Usuario;
 import com.luciano.controleservicos.services.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.validation.Valid;
@@ -15,6 +12,7 @@ import java.net.URI;
 
 @RestController
 @RequestMapping("/usuarios")
+@CrossOrigin("http://localhost:4200")
 public class UsuarioResource {
 
     @Autowired
